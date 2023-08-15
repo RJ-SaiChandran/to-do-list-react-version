d61ff46add967b7f48d94f1389e9f7ddf9caadd3
RJ SaiChandran <sai.cool877@gmail.com>
2023-08-15 13:44:21 +0530

    Initial commit

---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function ToDoItem(props) {
+  return (
+    <div>
+      <li
+        onClick={() => {
+          props.onChecked(props.id);
+        }}
+      >
+        {props.item}
+      </li>
+    </div>
+  );
+}
+
+export default ToDoItem;
